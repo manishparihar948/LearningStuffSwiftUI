f7d1625a6ffe2561354739b7ef8524199dd3168f
manishparihar948 <manish.parihar948@gmail.com>
2023-08-14 13:30:49 +0200

    Project Setup

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Manish Parihar on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
