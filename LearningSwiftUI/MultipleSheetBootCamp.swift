b4caf018934e13ba35499888af6aa7c5a17d6a56
manishparihar948 <manish.parihar948@gmail.com>
2023-08-15 12:53:01 +0200

    Multiple Sheet added

---
+++ LearningSwiftUI/MultipleSheetBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  MultipleSheetBootCamp.swift
+//  LearningSwiftUI
+//
+//  Created by Manish Parihar on 15.08.23.
+//
+
+import SwiftUI
+
+struct MultipleSheetBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MultipleSheetBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MultipleSheetBootCamp()
+    }
+}
