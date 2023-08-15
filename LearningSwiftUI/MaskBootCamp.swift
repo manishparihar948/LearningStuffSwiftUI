73dc1a9b3fbc9a18fd0f51328871448e7dba4cca
manishparihar948 <manish.parihar948@gmail.com>
2023-08-15 13:09:54 +0200

    Mask added

---
+++ LearningSwiftUI/MaskBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  MaskBootCamp.swift
+//  LearningSwiftUI
+//
+//  Created by Manish Parihar on 15.08.23.
+//
+
+import SwiftUI
+
+struct MaskBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MaskBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MaskBootCamp()
+    }
+}
