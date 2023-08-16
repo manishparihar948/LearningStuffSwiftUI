b14a556a2bb3a35fb61b4328e8ad160c75344695
manishparihar948 <manish.parihar948@gmail.com>
2023-08-16 18:06:41 +0200

    Weak Self concept with Reference Count

---
+++ LearningSwiftUI/WeakAndSelfBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  WeakAndSelfBootCamp.swift
+//  LearningSwiftUI
+//
+//  Created by Manish Parihar on 16.08.23.
+//
+
+import SwiftUI
+
+struct WeakAndSelfBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeakAndSelfBootCamp_Previews: PreviewProvider {
+    static var previews: some View {
+        WeakAndSelfBootCamp()
+    }
+}
